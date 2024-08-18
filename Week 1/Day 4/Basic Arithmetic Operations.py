7183834ec32c3e2c3386d0a6ee523ccaa8cb28dd
Bhaveshnagpure09 <bhaveshnagpure88@gmail.com>
2024-08-18 09:40:14 +0530

    Create Basic Arithmetic Operations.py

---
+++ Week 1/Day 4/Basic Arithmetic Operations.py
@@ -0,0 +1,14 @@
+# Project Title: Basic Arithmetic Operations
+
+# Function to perform basic arithmetic operations
+def basic_arithmetic_operations():
+    print("Basic Arithmetic Operations")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print(f"Addition: {num1 + num2}")
+    print(f"Subtraction: {num1 - num2}")
+    print(f"Multiplication: {num1 * num2}")
+    print(f"Division: {num1 / num2}")
+
+basic_arithmetic_operations()
