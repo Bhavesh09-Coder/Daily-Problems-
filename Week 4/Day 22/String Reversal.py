2000dca810a70459038bfcaffdbc91301947240e
Bhaveshnagpure09 <bhaveshnagpure88@gmail.com>
2024-08-20 18:25:12 +0530

    Create String Reversal.py

---
+++ Week 4/Day 22/String Reversal.py
@@ -0,0 +1,10 @@
+# Project Title: String Reversal
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print(f"Original String: {input_string}")
+print(f"Reversed String: {reversed_string}")
