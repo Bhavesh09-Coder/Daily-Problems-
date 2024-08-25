43dd97e64e840dab595e03af96b576a0102f2086
Bhaveshnagpure09 <bhaveshnagpure88@gmail.com>
2024-08-25 12:09:19 +0530

    Create Lambda Functions.py

---
+++ Week 5/Day 31/Lambda Functions.py
@@ -0,0 +1,15 @@
+# Project Title: Lambda Functions
+
+# Lambda function to add two numbers
+add = lambda x, y: x + y
+
+# Lambda function to square a number
+square = lambda x: x ** 2
+
+# Lambda function to check if a number is even
+is_even = lambda x: x % 2 == 0
+
+# Test the lambda functions
+print("Addition of 5 and 3 is:", add(5, 3))
+print("Square of 4 is:", square(4))
+print("Is 6 even?", is_even(6))
