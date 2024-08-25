b1a11f4504a8ba81748e69b3c7da6fcda9b93a32
Bhaveshnagpure09 <bhaveshnagpure88@gmail.com>
2024-08-25 12:06:14 +0530

    Create Simple Functions.py

---
+++ Week 5/Simple Functions/Simple Functions.py
@@ -0,0 +1,13 @@
+# Project Title: Simple Functions
+
+def greet(name):
+    """Function to greet a person by their name."""
+    return f"Hello, {name}!"
+
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
+
+# Test the functions
+print(greet("Alice"))
+print("Sum of 5 and 3 is:", add(5, 3))
